81b2980d4040e2cfcae564f25aab653890851071
wguilbert <williamguilbert@hotmail.com>
2025-03-26 15:06:12 -0400

    home page juste pour dire

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,6 +1,21 @@
-import { Title } from "@mantine/core";
+import { Box, Title } from "@mantine/core";
 
 const Home = () => {
-  return <Title order={1}>Home!</Title>;
+  return (
+    <Box
+      display="flex"
+      style={{
+        alignItems: "center",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
+      <Title order={1} style={{ marginBottom: 10 }}>
+        Welcome to FruityBlocks!
+      </Title>
+      <img src="/LogoFruity.png" draggable="false" width={300} height={300} />
+    </Box>
+  );
 };
+
 export default Home;
