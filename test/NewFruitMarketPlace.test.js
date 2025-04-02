c966019ebdb7b9379b3e3cfdfd7e0e81daf7b9dd
wguilbert <williamguilbert@hotmail.com>
2025-04-02 13:14:55 -0400

    test(): addFruit test

--- test/NewFruitMarketPlace.test.js
+++ test/NewFruitMarketPlace.test.js
@@ -1,6 +1,9 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+const FRUIT_NAME = "BANANA";
+const DEFAULT_PRICE = ethers.parseEther("1");
+
 describe("NewFruitMarketPlace", () => {
   let fruitContract;
   let owner, buyer;
@@ -10,6 +13,8 @@ describe("NewFruitMarketPlace", () => {
     const FruitMarket = await ethers.getContractFactory("NewFruitMarketPlace");
     fruitContract = await FruitMarket.deploy();
     await fruitContract.waitForDeployment();
+    await fruitContract.connect(owner).createUser();
+    await fruitContract.connect(buyer).createUser();
   });
 
   it("givenNewFruitMarketPlace_whenInstanceStart_shouldDeployContract", async () => {
@@ -17,7 +22,15 @@ describe("NewFruitMarketPlace", () => {
   });
 
   it("givenCreateUserRequest_whenUserIsNotRegistered_shouldRegisterUser", async () => {
-    await fruitContract.createUser();
     expect(await fruitContract.isRegistered()).to.equal(true);
+  });
+
+  it("givenAddFruitRequest_whenFruitDoesNotExist_shouldAddFruit", async () => {
+    await fruitContract.addFruit(FRUIT_NAME, DEFAULT_PRICE);
+    const addedFruit = await fruitContract.fruits(0);
+    expect(addedFruit.name).to.equal(FRUIT_NAME);
+    expect(addedFruit.price).to.equal(DEFAULT_PRICE);
+    expect(addedFruit.forSale).to.equal(false);
+    expect(addedFruit.owner).to.equal(await owner.getAddress());
   });
 });
